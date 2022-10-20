d9f46310c7de29d32cf9affbbe97d70a9d1db1e6
Bundit Hirunwongsawang <bundit.hir@gmail.com>
2022-10-21 03:17:55 +0700

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Nav from './Component/Nav';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="font-['Kumbh_Sans']">
+        <Nav />
+      </div>
+    )
+  }
+}
+
+export default App;
